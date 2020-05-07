41ba932f4b0fdedd275ed0753249d39762a1fcfa
Denny29 <dennyrfernandez2004@gmail.com>
2020-05-07 23:06:55 +0000

    Almost done, have to fix image to gif bug.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,27 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+let newUrl;
 
 $("#search-button").click(function(){
   
-  
-  
+    let confusing= $("#search-term").val()
+    let baseUrl="api.giphy.com/v1/gifs/search"
+    let apiKey="Uzwi0JC7HqS4B6Orz17aD4yKY1IB8IHj"
+
+    console.log(confusing);
+    newUrl=(`https://${baseUrl}?q=${confusing}&api_key=${apiKey}`);
+    console.log(newUrl);
+    fetch(newUrl)
+    .then(function(response) {
+  		return response.json();
+  	})
+    .then(function(response) {
+          console.log(response);
+          response.data[0].url
+          console.log(response.data[0].images['480w_still'].url);
+          $(".result").html(`<img id='result' src="${response.data[0].images['480w_still'].url}">`);
+  	})
+      
 });
 
